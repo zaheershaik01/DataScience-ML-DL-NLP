66d5f2793437a6cb63553c1a2d406dfca788310b
Shaik Zaheer <zaheershaik2925@gmail.com>
2026-02-17 11:10:23 +0530

    Add files via upload

---
+++ Flask Framework/app.py
@@ -0,0 +1,20 @@
+from flask import Flask
+'''
+It creates an instance of the Flask class,
+which will be your WSGI (Wev server Gateway Interface) application.
+'''
+
+### WSGI Application
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return "Welcome to this Flask course. This should be an amazing course."
+
+@app.route("/index")
+def index():
+    return "Welcome to the index page."
+
+if __name__ == "__main__":
+    app.run(debug=True)
